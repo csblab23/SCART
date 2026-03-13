216c49dbbbd2d737ab2a200d4d749d9aaab0c3ca
Vinaya20000 <shindevinaya0000@gmail.com>
2026-03-13 12:20:38 +0530

    Add setup.py to root

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+req_file = Path(__file__).parent / "requirements.txt"
+
+requirements = []
+if req_file.exists():
+    with open(req_file) as f:
+        requirements = f.read().splitlines()
 
 setup(
-    name="scT-CAR_Designer",
+    name="SCART",
     version="0.1.0",
-    description="Single-cell tumor CAR target design pipeline",
-    author="Your Name",
     packages=find_packages(),
-    install_requires=[
-        "GEOparse>=1.34.0",
-        "pandas>=1.3.0"
-    ],
+    install_requires=requirements,
     python_requires=">=3.8",
 )
-
