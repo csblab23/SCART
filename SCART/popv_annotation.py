fbc3bad6480aad26d335519991e8cc3b50db2116
Vinaya Shinde <160325451+Vinaya20000@users.noreply.github.com>
2026-04-09 15:47:43 +0530

    Update popv_annotation.py

--- SCART/popv_annotation.py
+++ SCART/popv_annotation.py
@@ -276,9 +276,9 @@ def run_popv_annotation(
         adata_processed = pq.adata
 
     if input_type == "raw":
-        methods = ["celltypist_on_scvi", "scanvi", "rf", "svm"]
+        methods = ["CELLTYPIST", "scanvi", "rf", "svm"]
     else:
-        methods = ["celltypist_on_scvi"]
+        methods = ["CELLTYPIST"]
     
     for m in methods:
         annotate_data(adata_processed, methods=[m])
