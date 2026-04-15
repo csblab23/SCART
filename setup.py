35ca4dcb52013aae9b0411038facc04961a739cd
Vinaya Shinde <160325451+Vinaya20000@users.noreply.github.com>
2026-04-15 15:16:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,34 +26,22 @@ setup(
     zip_safe=False,
 
 install_requires=[
-    # Core
     "scanpy==1.9.3",
     "scvi-tools==1.2.1",
     "scmalignantfinder==1.0.1",
 
-    #  FIXED JAX stack (compatible)
-    "jax==0.4.28",
-    "jaxlib==0.4.28",
-    "flax==0.10.2",
-    "optax==0.2.1",
-    "chex==0.1.7",
-
-    # Numerical
     "numpy==1.23.4",
     "pandas>=1.5",
     "scikit-learn>=1.2",
     "scipy>=1.9",
 
-    # ML
     "tensorflow==2.12.0",
     "torch>=2.0",
 
-    # Bio
     "geofetch==0.12.10",
     "GEOparse==2.0.4",
     "popv>=0.5",
 
-    # Utils
     "deap>=1.4",
     "rpy2==3.5.16",
     "typer",
