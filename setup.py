bb400674948f89848cf7b9e3cb73616c06c1d32c
Vinaya Shinde <160325451+Vinaya20000@users.noreply.github.com>
2026-04-17 16:16:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version="0.1.0",
     description="Single-cell Antigen Ranking Tool",
     author="CSB LAB",
-    packages=find_packages(),
+    packages=find_packages(include=["SCART", "SCART.*"]),
+
+
 
     python_requires=">=3.8",
 
